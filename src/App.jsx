edcbf7f7cbcc33f888bda20cd1dbb136dcc10097
Thiago Barbosa Soares <thiagosoares.barbosa@eb.mil.br>
2022-05-31 15:37:56 -0300

    23. ComponenteCard #02

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React from "react";
+import './App.css';
 
 import Card from "./components/layout/Card";
 import Aleatorio from "./components/basicos/Aleatorio";
@@ -8,26 +9,28 @@ import ComParametro from './components/basicos/ComParametro'
 import Fragmento from './components/basicos/Fragmento'
 
 export default _ =>
-    <div id="app">
+    <div className="App">
         <h1>Fundamento React</h1>
 
-        <Card titulo="#04 - Desafio Aleatório">
-            <Aleatorio min={10} max={60}></Aleatorio>
-        </Card>
+        <div className="Cards">
+            <Card titulo="#04 - Desafio Aleatório">
+                <Aleatorio min={10} max={60}></Aleatorio>
+            </Card>
 
-        <Card titulo="#03 - Fragmento">
-            <Fragmento />
-        </Card>
+            <Card titulo="#03 - Fragmento">
+                <Fragmento />
+            </Card>
 
-        <Card titulo="#02 - Com Parâmetro">
-            <ComParametro
-                titulo="Situação do Aluno"
-                aluno="Pedro Silva"
-                nota={8.7} />
-        </Card>
+            <Card titulo="#02 - Com Parâmetro">
+                <ComParametro
+                    titulo="Situação do Aluno"
+                    aluno="Pedro Silva"
+                    nota={8.7} />
+            </Card>
 
-        <Card titulo="#01 - Primeiro Componente">
-            <Primeiro />
-        </Card>
+            <Card titulo="#01 - Primeiro Componente">
+                <Primeiro />
+            </Card>
 
+        </div>
     </div>
