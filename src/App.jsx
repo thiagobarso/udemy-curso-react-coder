ada8640c7fef214959d19061b3e5b27aef45c0d4
Thiago Barbosa Soares <thiagosoares.barbosa@eb.mil.br>
2022-05-31 15:26:59 -0300

    22. ComponenteCard #01

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React from "react";
 
+import Card from "./components/layout/Card";
 import Aleatorio from "./components/basicos/Aleatorio";
 import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
@@ -9,11 +10,24 @@ import Fragmento from './components/basicos/Fragmento'
 export default _ =>
     <div id="app">
         <h1>Fundamento React</h1>
-        <Aleatorio min={10} max={60}></Aleatorio>
-        <Fragmento />
-        <ComParametro
-            titulo="Situação do Aluno"
-            aluno="Pedro Silva"
-            nota={8.7} />
-        <Primeiro />
+
+        <Card titulo="#04 - Desafio Aleatório">
+            <Aleatorio min={10} max={60}></Aleatorio>
+        </Card>
+
+        <Card titulo="#03 - Fragmento">
+            <Fragmento />
+        </Card>
+
+        <Card titulo="#02 - Com Parâmetro">
+            <ComParametro
+                titulo="Situação do Aluno"
+                aluno="Pedro Silva"
+                nota={8.7} />
+        </Card>
+
+        <Card titulo="#01 - Primeiro Componente">
+            <Primeiro />
+        </Card>
+
     </div>
