989283155c0c076b914476327e77d465fadb4df2
Thiago Barbosa Soares <thiagosoares.barbosa@eb.mil.br>
2022-05-31 15:44:35 -0300

    24. ComponenteCard #03

--- src/App.jsx
+++ src/App.jsx
@@ -13,22 +13,22 @@ export default _ =>
         <h1>Fundamento React</h1>
 
         <div className="Cards">
-            <Card titulo="#04 - Desafio Aleatório">
+            <Card titulo="#04 - Desafio Aleatório" color="#FA6900">
                 <Aleatorio min={10} max={60}></Aleatorio>
             </Card>
 
-            <Card titulo="#03 - Fragmento">
+            <Card titulo="#03 - Fragmento" color="#E94C6F">
                 <Fragmento />
             </Card>
 
-            <Card titulo="#02 - Com Parâmetro">
+            <Card titulo="#02 - Com Parâmetro" color="#E8B71A">
                 <ComParametro
                     titulo="Situação do Aluno"
                     aluno="Pedro Silva"
                     nota={8.7} />
             </Card>
 
-            <Card titulo="#01 - Primeiro Componente">
+            <Card titulo="#01 - Primeiro Componente" color="#588C73">
                 <Primeiro />
             </Card>
 
--- src/components/layout/Card.jsx
+++ src/components/layout/Card.jsx
@@ -2,8 +2,14 @@ import React from "react";
 import "./Card.css";
 
 const Card = (props) => {
+
+    const cardStyle = {
+        backgroundColor : props.color || '#0F0',
+        borderColor: props.color || '#F00'
+    }
+
     return (
-        <div className="Card">
+        <div className="Card" style={cardStyle}>
             <div className="Title">{props.titulo}</div>
             <div className="Content">
                 {props.children}
