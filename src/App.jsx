754c86946490e923c1981871d71ee99422e58ccc
Thiago Barbosa Soares <thiagosoares.barbosa@eb.mil.br>
2022-05-31 14:39:02 -0300

    19. Componente App

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
+import Fragmento from './components/basicos/Fragmento'
+
+export default _ =>
+    <div id="app">
+        <h1>Fundamento React</h1>
+        <Fragmento />
+        <ComParametro
+            titulo="Situação do Aluno"
+            aluno="Pedro Silva"
+            nota={8.7} />
+        <Primeiro />
+    </div>
--- src/index.js
+++ src/index.js
@@ -2,17 +2,9 @@ import './index.css'
 import ReactDOM from 'react-dom'
 import React from 'react'
 
-import Primeiro from './components/basicos/Primeiro'
-import ComParametro from './components/basicos/ComParametro'
-import Fragmento from './components/basicos/Fragmento'
+import App from './App'
 
 ReactDOM.render(
-    <div id="app">
-        <Primeiro />
-        <ComParametro
-            titulo="Situação do Aluno"
-            aluno="Pedro Silva"
-            nota={8.7} />
-        <Fragmento />
-    </div>,
-    document.getElementById('root'))
+    <App />,
+    document.getElementById('root')
+)
