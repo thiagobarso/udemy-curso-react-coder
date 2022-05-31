38efaa87e0efe958f1bec86f79b4fcfc9aa2abfb
Thiago Barbosa Soares <thiagosoares.barbosa@eb.mil.br>
2022-05-31 14:01:53 -0300

    15. Primeiro Component #02

--- src/components/basicos/Primeiro.js
+++ src/components/basicos/Primeiro.js
@@ -1,3 +1,11 @@
-export default function Primeiro(){
-    return 'Primeiro Componente'
+import React from 'react';
+
+export default function Primeiro() {
+    const msg = 'Seja bem vindo(a)!'
+    return (
+        <div>
+            <h2>Primeiro Componente</h2>
+            <p>{msg}</p>
+        </div>
+    )
 }
