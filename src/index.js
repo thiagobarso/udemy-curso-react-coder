95d040dfea08ffe406cf1ebcd29ea0d37461c9a3
Thiago Barbosa Soares <thiagobarso@gmail.com>
2022-06-02 23:27:03 -0300

    44. Componente Calculator

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import './index.css'
 import React from 'react'
 import ReactDOM from "react-dom/client";
 
-import App from './App'
+import Calculator from './main/Calculator';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
     <React.StrictMode>
-        <App />
+    <div>
+        <h1>Calculator</h1>
+        <Calculator />
+    </div>
     </React.StrictMode>
 );
 
