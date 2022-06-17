ea5a135e7c83a625930ee9c9c157c708b57ef39e
Thiago Barbosa Soares <thiagobarso@gmail.com>
2022-06-17 12:22:12 -0300

    87. Integração JS/JSX

--- aulao_nextjs/fundamentos/src/pages/index.jsx
+++ aulao_nextjs/fundamentos/src/pages/index.jsx
@@ -1,7 +1,7 @@
 export default function Inicio(){
     return (
         <div>
-            <h1>Fundamento de Next.js</h1>
+            <h1>Fundamento de Next.js & React</h1>
             <h2>Vamos estudar esse framework?</h2>
         </div>
     )
