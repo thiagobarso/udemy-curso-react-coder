08e5933598557ad90d4b12eb395786468554b70f
Thiago Barbosa Soares <thiagobarso@gmail.com>
2022-06-21 16:24:16 -0300

    92. Navegação entre Componentes

--- aulao_nextjs/fundamentos/src/pages/estiloso.jsx
+++ aulao_nextjs/fundamentos/src/pages/estiloso.jsx
@@ -1,8 +1,12 @@
 import styles from '../styles/Estiloso.module.css'
+import Link from 'next/link'
 
 export default function Estiloso(){
     return (
         <div className={styles.roxo}>
+            <Link href="/">
+                Voltar
+            </Link>
             <h1>Estilo usando CSS Módulos</h1>
         </div>
     )
