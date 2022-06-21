a82b934546113c6b83e2c96f4007d8076943e939
Thiago Barbosa Soares <thiagobarso@gmail.com>
2022-06-21 17:18:09 -0300

    97. Componente com Estado

--- aulao_nextjs/fundamentos/src/pages/index.jsx
+++ aulao_nextjs/fundamentos/src/pages/index.jsx
@@ -13,7 +13,8 @@ export default function Inicio(){
             <Navegador texto="Exemplo" destino="/exemplo" cor="#9400d3"/>
             <Navegador texto="JSX" destino="/jsx" cor="crimson"/>
             <Navegador texto="Navegação #01" destino="/navegacao" cor="green"/>
-            <Navegador texto="Navegação #02" destino="/cliente/sp-2/321" cor="green"/>
+            <Navegador texto="Navegação #02" destino="/cliente/sp-2/321" cor="blue"/>
+            <Navegador texto="Componente com Estado" destino="/estado" cor="pink"/>
         </div>
     )
 }
