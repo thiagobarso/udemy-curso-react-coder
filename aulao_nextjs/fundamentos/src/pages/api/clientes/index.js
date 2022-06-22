893d4f6b32ac4f4d93602d0f5bf2a79d84bb5708
Thiago Barbosa Soares <thiagobarso@gmail.com>
2022-06-22 10:50:04 -0300

    98. Usando API #01

---
+++ aulao_nextjs/fundamentos/src/pages/api/clientes/index.js
@@ -0,0 +1,17 @@
+export default function handler(req, res) {
+    if (req.method === "GET") {
+        handleGet(req, res)
+    } else {
+        res.status(405).send()
+    }
+
+    return res.status(200).send()
+}
+
+function handleGet(req, res) {
+    res.status(200).json({
+        id: 3,
+        nome: 'Maria',
+        email: 'maria@gmail.com'
+    })
+}
