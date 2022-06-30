ac8a68bad2870920ac28856d1051ffd53c5e0480
Thiago Barbosa Soares <thiagobarso@gmail.com>
2022-06-30 11:48:05 -0300

    138. Configurando as Rotas (React-Router)

--- todo-app/frontend/src/template/menu.jsx
+++ todo-app/frontend/src/template/menu.jsx
@@ -11,7 +11,7 @@ export default props => (
         </div>
         <div className="navbar-collapse collapse">
             <ul className="nav navbar-nav">
-                <li><a href="#/todos">Tarefas</a></li>
+                <li><a href="#/todo">Tarefas</a></li>
                 <li><a href="#/about">Sobre</a></li>
             </ul>
         </div>
