12dfb280ab3656c1e292c2f08f8786bff802cd98
Thiago Barbosa Soares <thiagobarso@gmail.com>
2022-07-12 16:47:47 -0300

    169. Refatorando Reducer

--- redux-simples/src/store/storeConfig.js
+++ redux-simples/src/store/storeConfig.js
@@ -1,27 +1,9 @@
 import { createStore, combineReducers } from 'redux'
 
-const reducers = combineReducers({
-    numeros: function (state, action) {
+import numerosReducer from './reducers/numeros'
 
-        switch (action.type) {
-            case 'NUM_MIN_ALTERADO':
-                return {
-                    ...state,
-                    min: action.payload
-                }
-            case 'NUM_MAX_ALTERADO':
-                return {
-                    ...state,
-                    max: action.payload
-                }
-            default: {
-                return {
-                    min: 7,
-                    max: 31
-                }
-            }
-        }
-    },
+const reducers = combineReducers({
+    numeros: numerosReducer,
     nomes: function (state, action) {
 
         return [
