54ef6fbdcb6d004da1a883c32014c50acab572cc
Thiago Barbosa Soares <thiagobarso@gmail.com>
2022-07-12 16:15:08 -0300

    168. Invocando a Action Creator #03

--- redux-simples/src/components/Intervalo.jsx
+++ redux-simples/src/components/Intervalo.jsx
@@ -2,24 +2,24 @@ import './Intervalo.css'
 import React from 'react'
 import { connect } from 'react-redux'
 import Card from './Card'
-import { alterarNumeroMinimo } from '../store/actions/numeros'
+import { alterarNumeroMinimo, alterarNumeroMaximo } from '../store/actions/numeros'
 
 function Intervalo(props) {
 
     const { min, max } = props
-
-    props.alterarMinimo(1000)
 
     return (
         <Card title="Intervalo de Números" red>
             <div className='Intervalo'>
                 <span>
                     <strong>Mínimo:</strong>
-                    <input type="number" value={min} readOnly />
+                    <input type="number" value={min}
+                        onChange={e => props.alterarMinimo(+e.target.value)} />
                 </span>
                 <span>
                     <strong>Máximo:</strong>
-                    <input type="number" value={max} readOnly />
+                    <input type="number" value={max}
+                        onChange={e => props.alterarMaximo(+e.target.value)} />
                 </span>
             </div>
         </Card>
@@ -33,14 +33,19 @@ function mapStateToProps(state) {
     }
 }
 
-function mapActionCreatorsToProp(dispatch) {
+function mapDispatchToProps(dispatch) {
     return {
         alterarMinimo(novoNumero) {
             //action creator -> action
             const action = alterarNumeroMinimo(novoNumero)
             dispatch(action)
+        },
+        alterarMaximo(novoNumero) {
+            //action creator -> action
+            const action = alterarNumeroMaximo(novoNumero)
+            dispatch(action)
         }
     }
 }
 
-export default connect(mapStateToProps, mapActionCreatorsToProp)(Intervalo)
+export default connect(mapStateToProps, mapDispatchToProps)(Intervalo)
