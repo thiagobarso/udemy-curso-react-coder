aa869f73c56d38c2bf68b6cd2f687bd3bae54be6
Thiago Barbosa Soares <thiagobarso@gmail.com>
2022-07-12 16:58:12 -0300

    170. Constantes para os Tipos das Actions

--- redux-simples/src/store/actions/numeros.js
+++ redux-simples/src/store/actions/numeros.js
@@ -1,13 +1,15 @@
-//Action Creator
-export function alterarNumeroMinimo(novoNumero){
+import { NUM_MAX_ALTERADO, NUM_MIN_ALTERADO } from './actionTypes'
+
+
+export function alterarNumeroMinimo(novoNumero) {
     return {
-        type: 'NUM_MIN_ALTERADO',
+        type: NUM_MIN_ALTERADO,
         payload: novoNumero
     }
 }
-export function alterarNumeroMaximo(novoNumero){
+export function alterarNumeroMaximo(novoNumero) {
     return {
-        type: 'NUM_MAX_ALTERADO',
+        type: NUM_MAX_ALTERADO,
         payload: novoNumero
     }
 }
