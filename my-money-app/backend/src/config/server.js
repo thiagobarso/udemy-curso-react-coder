9e96fd1baf8e15d6ab2af6ca8f550f31b9fb20e8
Thiago Barbosa Soares <thiagobarso@gmail.com>
2022-07-17 23:58:14 -0300

    203. Middleware Query Parser Int - faltando ()

--- my-money-app/backend/src/config/server.js
+++ my-money-app/backend/src/config/server.js
@@ -9,7 +9,7 @@ const queryParser = require('express-query-int')
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 server.use(allowCors)
-server.use(queryParser)
+server.use(queryParser())
 
 server.listen(port, function () {
     console.log(`BACKEND is running on port ${port}`)
